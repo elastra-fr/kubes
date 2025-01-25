9e0da2cf560ffdd91b237e71036397acdc0d188f
Emmanuel LASTRA <emmanuellastra@gmail.com>
2025-01-25 18:36:20 +0100

    Initial

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,63 @@
+"use client"
+import {useState} from "react";
+
+
+
+export default function Header({onKubeChange}){ {
+
+const [selectedKube, setSelectedKube] = useState("");
+
+
+const handleSelectedKubeChange = (e) => {
+
+setSelectedKube(e.target.value);
+onKubeChange(e.target.value);
+
+}
+
+return(
+
+<>
+
+<header className="flex justify-between items-center p-4 bg-gray-800 text-white">
+
+<div>
+
+<span className="text-xl">Kubes</span>
+
+</div>
+
+<div>
+
+<select className="p-2 bg-gray-900 text-white rounded-md"
+value={selectedKube}
+onChange={handleSelectedKubeChange}
+>
+
+<option>Kube 1</option>
+<option>Kube 2</option>
+
+</select>
+
+
+</div>
+
+
+
+</header>
+
+
+
+</>
+
+
+
+);
+
+
+
+
+
+}
+
+}
---
+++ src/components/kube2/Kube2.jsx
@@ -0,0 +1,30 @@
+"use client";
+
+import styles from "./Kube2.module.css";
+import {useState} from "react";
+
+export default function Cube() {
+    const [isExploded, setIsExploded] = useState(false);
+
+    return (
+        <>
+            <button 
+                id={styles.cubeButton} 
+                className="rounded bg-black text-white p-4"
+            >
+                Explode Cube
+            </button>
+
+            <div className={styles.cubeContainer}>
+                <div className={styles.cubeBox} id={styles.cubeBox2}>
+                    <div className={styles.cubeCard} id={styles.front}>Front</div>
+                    <div className={styles.cubeCard} id={styles.back}>Back</div>    
+                    <div className={styles.cubeCard} id={styles.left}>Left</div>
+                    <div className={styles.cubeCard} id={styles.right}>Right</div>
+                    <div className={styles.cubeCard} id={styles.top}>Top</div>
+                    <div className={styles.cubeCard} id={styles.bottom}>Bottom</div>
+                </div>
+            </div>
+        </>
+    );
+}
