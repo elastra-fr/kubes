10c1c17f04bbeca3f9c99c24b266cc26eb0df27e
Emmanuel LASTRA <emmanuellastra@gmail.com>
2025-01-27 16:03:44 +0100

    Création d'un carousel 3D

--- src/components/kube3/Kube3.jsx
+++ src/components/kube3/Kube3.jsx
@@ -17,12 +17,35 @@ export default function Cube() {
 
             <div className={styles.cubeContainer}>
                 <div className={styles.cubeBox} id={styles.cubeBox}>
-                    <div className={styles.cubeCard} id={styles.front}>Front</div>
-                    <div className={styles.cubeCard} id={styles.back}>Back</div>    
-                    <div className={styles.cubeCard} id={styles.left}>Left</div>
-                    <div className={styles.cubeCard} id={styles.right}>Right</div>
-                    <div className={styles.cubeCard} id={styles.top}>Top</div>
-                    <div className={styles.cubeCard} id={styles.bottom}>Bottom</div>
+                    <div className={styles.cubeCard} id={styles.front}>
+                        <img 
+                            src="https://picsum.photos/300/300?random=1" 
+                            alt="front"
+                            className={styles.faceImage}
+                        />
+
+                    </div>
+                    <div className={styles.cubeCard} id={styles.back}>
+                        <img 
+                            src="https://picsum.photos/300/300?random=2" 
+                            alt="back"
+                            className={styles.faceImage}
+                        />
+                    </div>    
+                    <div className={styles.cubeCard} id={styles.left}>
+                        <img 
+                            src="https://picsum.photos/300/300?random=3" 
+                            alt="left"
+                            className={styles.faceImage}
+                        />
+                    </div>
+                    <div className={styles.cubeCard} id={styles.right}>
+                        <img 
+                            src="https://picsum.photos/300/300?random=4" 
+                            alt="right"
+                            className={styles.faceImage}
+                        />
+                    </div>
                 </div>
             </div>
         </>
